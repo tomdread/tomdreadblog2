2572024d9365578e07fd1d224fdd58746aab83bd
Thomas Rooney <tom@dreadserver.com>
2018-07-15 21:53:10 +0100

    Fucking around

---
+++ components/MainNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="nav">
+    <ul class="navLinks">
+      <li class="link">
+        <a href="/">Home</a>
+      </li>
+      <li class="link">
+        <a href="/about">About</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
